066606095de0f01676e210a6c18b5eeac41f8bed
Weihonghao <SCIN@DN0a22a08e.SUNet>
2017-05-15 15:04:19 -0700

    data

--- filter_data.py
+++ filter_data.py
@@ -7,17 +7,23 @@ analyzer = SentimentIntensityAnalyzer()
 old_line = ""
 
 text_file = open(write_file_name, "w")
+question_file = open('question.txt','w')
+answer_file = open('answer.txt','w')
 
 with open(source_file_name) as file:
     for line in file:
         sentiment_result = analyzer.polarity_scores(old_line)
         sentiment_result2 = analyzer.polarity_scores(line)
-        if (sentiment_result['compound'] < -0.4) and (sentiment_result2['compound'] > 0.4) and (len(old_line) < 100) and (len(line) < 100):
+        if (sentiment_result['compound'] < -0.4) and (sentiment_result2['compound'] > 0.4) and (len(old_line) < 70) and (len(line) < 70):
             text_file.write("--------------------\n")
             text_file.write(old_line)
+            question_file.write(old_line)
             text_file.write(line)
+            answer_file.write(line)
             text_file.write("--------------------\n")
 
         old_line = line
 
 text_file.close()
+question_file.close()
+answer_file.close()
